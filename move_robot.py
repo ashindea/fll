3293bea2ac26febe5745e465957ea1c49388c486
ashindea <59342670+ashindea@users.noreply.github.com>
2019-12-29 19:46:53 -0500

    Update move_robot.py

--- move_robot.py
+++ move_robot.py
@@ -78,8 +78,77 @@ def move_to_obstacle(robot,
 
 
 def follow_line(robot,
+    color_sensor,
+    max_distance = 0, 
+    stop_on_color=None,
+    speed_mm_s = 100):
+
+    # PID tuning
+    Kp = 1  # proportional gain
+    Ki = 0  # integral gain
+    Kd = 0  # derivative gain
+    target_value = color_sensor.reflection()
+    interval = 100 # millisecpnds to sample
+    
+    max_duration = 1000 * int(max_distance / speed_mm_s)
+    cum_duration = 0
+    error_increasing_cnt = 0
+    integral = 0
+    previous_error = 0
+
+    while true:
+        # Calculate steering using PID algorithm
+        error = target_value - color_sensor.reflection()
+        integral += (error * interval)
+        derivative = (error - previous_error) / interval
+
+        # u zero:     on target,  drive forward
+        # u positive: too bright, turn right
+        # u negative: too dark,   turn left
+        u = (Kp * error) + (Ki * integral) + (Kd * derivative)
+        print(' previous_error ' + previous_error
+            + ' error ' + error
+            + ' integral ' + integral
+            + ' derivative ' + derivative
+            + ' u ' + u
+            + ' error_increasing_cnt ' + error_increasing_cnt
+            + ' cum_distance ' + (cum_duration * speed_mm_s)/1000
+        )
+
+        robot.drive(speed_mm_s, u)
+        wait(interval)
+        cum_duration += interval
+
+        # Check any endng conditions being met
+        if (max_distance > 0 and cum_duration >= max_duration)
+            or 
+            (stop_on_color and color_sensor.color() == stop_on_color):
+            robot.stop(stop_type=Stop.BRAKE)
+            print('Stopping as end met')
+            return true
+ 
+        # If error increasing too much quit
+        if error * previous_error > 0 and abs(error) >= abs(previous_error):
+            error_increasing_cnt += 1
+            if error_increasing_cnt > 5:
+                robot.stop(stop_type=Stop.BRAKE)
+                print('Too much Error , quitting')
+                return false
+                
+
+
+        previous_error = error
+
+
+
+def follow_line_old(robot,
+    left_motor, 
+    right_motor, 
+    color_sensor,
     color_sensor_left,
     color_sensor_right,
+    max_distance = 0, 
+    stop_on_color=None,
     speed_mm_s = 100):
 
     if ( color_sensor_left.color() != color_sensor_right.color()):
