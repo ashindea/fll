bffde30b1d03208dcb6b13278a9d054da08f81f8
ashindea <59342670+ashindea@users.noreply.github.com>
2019-12-29 19:46:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,32 @@ import worker
 import sensor1
 
 
+def do_color_dance(robot, color_sensor_left):
+    move_robot.move_to_color(robot=robot,color_sensor=color_sensor_left,
+        stop_on_color=Color.RED)
+
+    move_robot.move_to_color_reverse(robot=robot,color_sensor=color_sensor_left,
+        stop_on_color=Color.BLACK)
+
+def do_crane_dance(robot, crane_motor):
+    move_robot.move_straight(robot=robot, max_distance=100)
+    move_robot.turn(robot=robot, angle=90)
+    worker.move_crane_down(robot, crane_motor, degrees=45)
+    worker.move_crane_up(robot, crane_motor, degrees=45)
+    move_robot.move_straight(robot=robot, max_distance=-100)
+    move_robot.turn(robot=robot, angle=180)
+    move_robot.move_straight(robot=robot, max_distance=150)
+    worker.move_crane_down(robot, crane_motor, degrees=60)
+    worker.move_crane_up(robot, crane_motor, degrees=60)
+
+
+def do_line_follow(robot, color_sensor):
+    follow_line(robot = robot,
+        color_sensor = color_sensor,
+        max_distance = 0, 
+        stop_on_color=None,
+        speed_mm_s = 100)
+
 #
 # Write your program here
 brick.sound.beep(1000, 600)
@@ -27,19 +53,9 @@ brick.display.text('Starting')
 
 robot, crane_motor, left_motor, right_motor,color_sensor_left, color_sensor_right=setup_robot.get_robot()
 
-#move_robot.move_to_color(robot=robot,color_sensor=color_sensor_left,
-#    stop_on_color=Color.RED)
-
-#move_robot.move_to_color_reverse(robot=robot,color_sensor=color_sensor_left,
-#    stop_on_color=Color.BLACK)
-
-move_robot.move_straight(robot=robot, max_distance=100)
-move_robot.turn(robot=robot, angle=90)
-worker.move_crane_down(robot, crane_motor, degrees=45)
-worker.move_crane_up(robot, crane_motor, degrees=45)
-move_robot.move_straight(robot=robot, max_distance=-100)
-move_robot.turn(robot=robot, angle=180)
-worker.move_crane_down(robot, crane_motor, degrees=90)
+#do_crane_dance(robot, crane_motor)
+#do_color_dance(robot, color_sensor_left)
+do_line_follow(robot, color_sensor=color_sensor_left)
 
 wait(20000)
 
